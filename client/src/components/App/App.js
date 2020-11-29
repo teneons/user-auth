793ccbf24c51e69374db45184f4557ea668c82ce
teneons <teneons@gmail.com>
2020-11-29 13:12:21 +0200

    impl. gets data by form

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import {routes} from '../routes/routes';
 
 function App() {
-  const rts = routes(false)
+  const rts = routes(true)
   return (
     <div className="App">
       <Router>
