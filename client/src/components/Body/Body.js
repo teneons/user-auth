ee477ae82eab8fb87d25c2c08f5787fa6a48c37e
teneons <teneons@gmail.com>
2020-11-29 14:37:42 +0200

    corrected syntax

--- client/src/components/Body/Body.js
+++ client/src/components/Body/Body.js
@@ -10,11 +10,11 @@ export default class Body extends Component {
         <div className='container col-4 d-flex justify-content-center align-content-center rounded-3 bg-dark shadow-lg' >
           
           <div className='m-2'>
-            <a href="/signup" className='btn btn-lg btn-outline-light fw-bold text-decoration-none'>Sign <span class="badge bg-light text-dark text-decoration-underline">UP</span></a>
+            <a href="/signup" className='btn btn-lg btn-outline-light fw-bold text-decoration-none'>Sign <span className="badge bg-light text-dark text-decoration-underline">UP</span></a>
           </div>
           <span className='text-white display-6 m-1'>/</span>
           <div className='m-2'>
-            <a href="/signin" className='btn btn-lg btn-outline-light fw-bold text-decoration-none'>Sign <span class="badge bg-light text-dark text-decoration-underline">IN</span></a>
+            <a href="/signin" className='btn btn-lg btn-outline-light fw-bold text-decoration-none'>Sign <span className="badge bg-light text-dark text-decoration-underline">IN</span></a>
           </div>
         
         </div>
